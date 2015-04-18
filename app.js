7f698e78c117af465908992c166c73f5b538f871
M. Hilton <meredithjunior@gmail.com>
2015-04-18 12:46:06 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// just for the demos, avoids form submit
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
+$( "#myform" ).validate({
+  rules: {
+    firstname: {
+      required: true
+    }
+  }
+});
